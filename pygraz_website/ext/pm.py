d22ea3807459e3221116bb6e21734ef37eb98cba
Horst Gutmann <zerok@zerokspot.com>
2012-03-21 23:15:08 -0400

    Integrates flask-postmark

--- pygraz_website/ext/pm.py
+++ pygraz_website/ext/pm.py
@@ -1,47 +1,17 @@
-import postmark
 import unittest
-import warnings
 import logging
+from flask_postmark import Postmark as BasePostmark
 
 
 logger = logging.getLogger(__name__)
 
 
-class Postmark(object):
-    """
-    A simple postmark loader that prefills mail objects with things like the api key.
-    """
-
-    def __init__(self, app=None):
-        self.app = app
-        if self.app is not None:
-            self.init_app(app)
-
-    def init_app(self, app):
-        self.app = app
-        if 'POSTMARK_API_KEY' not in self.app.config:
-            warnings.warn("POSTMARK_API_KEY not set in the configuration!")
-
-    def get_config(self, key):
-        return self.app.config.get('POSTMARK_{}'.format(key))
-
-    def create_mail(self, *args, **kwargs):
-        test_mode = kwargs.get('test_mode', None)
-        if test_mode is None:
-            test_mode = self.get_config('TEST_MODE')
-        if test_mode:
-            mail = PMTestMail(*args, **kwargs)
-        else:
-            mail = postmark.PMMail(*args, **kwargs)
-        mail.api_key = self.get_config('API_KEY')
-        if 'sender' not in kwargs:
-            mail.sender = self.get_config('SENDER')
-        return mail
+class Postmark(BasePostmark):
 
     def send_bcc_batch(self, mail, recipients):
         """
         This method basically sends a batch of mails by setting the
-        recipients via the bcc header. It does not touch the bcc 
+        recipients via the bcc header. It does not touch the bcc
         header.
         """
         if len(recipients) == 1:
@@ -53,12 +23,6 @@ class Postmark(object):
             mail.bcc = ','.join(recs)
             logger.debug("Sending mail to {}".format(mail.bcc))
             mail.send()
-
-
-class PMTestMail(postmark.PMMail):
-    def send(self, *args, **kwargs):
-        kwargs['test'] = True
-        return super(PMTestMail, self).send(*args, **kwargs)
 
 
 def _split_recipients(recipients, max_num=20):
